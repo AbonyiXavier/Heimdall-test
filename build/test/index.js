013c9f549e46ab272d66f97bb14d58f6ccf753fe
AbonyiXavier <francisabonyi@gmail.com>
2020-11-25 22:01:35 +0100

    heroku hosting

--- build/test/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-require("./server.spec");
-
-require("./controllers/questionOne.spec");
-
-require("./controllers/questionTwo.spec");
-
-require("./controllers/questionThree.spec");
