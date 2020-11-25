ac9d8be7a45a8ee6090d7a2865a5e6dbb86fff52
AbonyiXavier <francisabonyi@gmail.com>
2020-11-25 10:58:50 +0100

    setup base and first route

---
+++ .history/src/index_20201125101727.js
@@ -0,0 +1,22 @@
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (request, response) => {
+  response.status(200).json({
+    status: true,
+    message: "Welcome to Heimdall Technologies APIs Test questions....",
+  });
+});
+
+export default app;
