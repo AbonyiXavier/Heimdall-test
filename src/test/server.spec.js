8eaa184da36b567608edaab6179dd3499bf80a3a
AbonyiXavier <francisabonyi@gmail.com>
2020-11-25 11:18:43 +0100

    mocha and chai test for the homepage route

---
+++ src/test/server.spec.js
@@ -0,0 +1,24 @@
+import chai, { expect } from "chai";
+import chaiHttp from "chai-http";
+import app from "../index";
+
+chai.should();
+
+chai.use(chaiHttp);
+
+describe("/ should display Welcome to Heimdall Technologies APIs Test questions....", () => {
+  it("it should get the home page", (done) => {
+    chai
+      .request(app)
+      .get("/")
+      .end((error, response) => {
+        expect(response).to.have.status(200);
+        expect(response.body.message).to.equal(
+          "Welcome to Heimdall Technologies APIs Test questions...."
+        );
+        done();
+      });
+  });
+});
+
+export default describe;
