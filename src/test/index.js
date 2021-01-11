9f675a42e55164bfd85514e9044a1885e10395db
XavierAbonyi <francis.abonyi@scelloo.com>
2021-01-11 15:57:47 +0100

    changes

--- src/test/index.js
+++ src/test/index.js
@@ -1,4 +1,4 @@
 import "./server.spec";
 import "./controllers/questionOne.spec";
-import "./controllers/questionTwo.spec";
 import "./controllers/questionThree.spec";
+import "./controllers/questionTwo.spec";
